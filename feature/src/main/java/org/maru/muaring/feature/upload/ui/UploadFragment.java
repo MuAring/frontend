50ce0f54e1424436c928d49daf01bec9853dc8cc
yyen <brenda1217@naver.com>
2025-12-07 05:20:50 +0900

    #81 [feat]: 오늘의 음악 업로드 페이지

--- feature/src/main/java/org/maru/muaring/feature/upload/ui/UploadFragment.java
+++ feature/src/main/java/org/maru/muaring/feature/upload/ui/UploadFragment.java
@@ -1,4 +1,12 @@
 package org.maru.muaring.feature.upload.ui;
 
-public class UploadFragment {
+import androidx.fragment.app.Fragment;
+
+import org.maru.muaring.feature.R;
+
+public class UploadFragment extends Fragment {
+
+    public UploadFragment() {
+        super(R.layout.fragment_upload);
+    }
 }
